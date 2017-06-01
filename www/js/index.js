431dcfb98e7f00376fda143ed43fa05eae341708
Volker Zahn <volker.zahn@t-systems.com>
2017-06-01 17:31:56 +0200

    Alert changed to console output

--- www/js/index.js
+++ www/js/index.js
@@ -55,14 +55,14 @@ var Longitude = undefined;
 // current GPS coordinates
 //
 var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+    console.log('Latitude: '          + position.coords.latitude          + '\n' +
+                'Longitude: '         + position.coords.longitude         + '\n' +
+                'Altitude: '          + position.coords.altitude          + '\n' +
+                'Accuracy: '          + position.coords.accuracy          + '\n' +
+                'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                'Heading: '           + position.coords.heading           + '\n' +
+                'Speed: '             + position.coords.speed             + '\n' +
+                'Timestamp: '         + position.timestamp                + '\n');
     
     var element = document.getElementById('geolocation');
     element.innerHTML = 'Lat/Lon: '  + position.coords.latitude + ' ' + position.coords.longitude + '<br />';
