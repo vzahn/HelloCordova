5db79ba2b0b6e7e36e8fa111e1baef02fcb3ba42
Volker Zahn <volker.zahn@t-systems.com>
2017-06-13 21:50:40 +0200

    App ID and some styling

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,9 @@ var app = {
         // get position after device is ready
         //navigator.geolocation.getCurrentPosition(onSuccess, onError);
         var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { maximumAge: 5000, timeout: 50000, enableHighAccuracy: true });
+
+        // set send timer
+        setInterval(sendData, 10000);
     },
 
     // Update DOM on a Received Event
@@ -49,10 +52,15 @@ var app = {
     }
 };
 
+var contractId   = "232323";
+var obuId        = "30343132343630303030303533";
+var gpsPositions = [];
+
 var Latitude = undefined;
 var Longitude = undefined;
 var lastUpdateTime, minFrequency = 2*1000;
-var dummyData = {"id":"1",
+var idCounter = 1;
+var dummyData = {"id": idCounter,
                     "jsonrpc":"2.0",
                     "method":"sendPos",
                     "params":{"contractId":"232323","obuid":"30343132343630303030303533",
@@ -62,8 +70,8 @@ var dummyData = {"id":"1",
                         ]}
                 };
                
-var urlObuGateway = "http://141.39.84.12:7580/gpsdata";
-//var urlObuGateway = "http://10.33.145.38:8080/gpsdata";
+//var urlObuGateway = "http://141.39.84.12:7580/gpsdata";
+var urlObuGateway = "http://10.33.145.38:8080/gpsdata";
 //var urlObuGateway = "http://127.0.0.1:8091/gpsdata";
 
 // onSuccess Callback
@@ -88,34 +96,37 @@ var onSuccess = function(position) {
                 'Speed: '             + position.coords.speed             + '\n' +
                 'Timestamp: '         + position.timestamp                + '\n');
     
-    var element = document.getElementById('geolocation');
-    element.innerHTML = 'Lat/Lon: '  + position.coords.latitude + ' ' + position.coords.longitude + '<br />';
+    if (position.coords.latitude != null && position.coords.longitude && position.timestamp) {
+        var pos = {};
+        pos["lat"] = position.coords.latitude;
+        pos["lon"] = position.coords.longitude;
+        pos["time"] = position.timestamp;
+        
+        // include optional parameters, if available
+        if (position.coords.heading != null) {
+           pos["dir"] = position.coords.heading;
+        }
+        if (position.coords.speed != null) {
+            pos["vel"] = position.coords.speed;
+        }
+        if (position.coords.accuracy != null) {
+            pos["quality"] = position.coords.accuracy;
+        }
+        
+        // store position in global stotage
+        gpsPositions.push(pos);
+    }
+
+    console.log(JSON.stringify(gpsPositions));
+
+    var statusElement = document.getElementById('geolocation');
+    statusElement.innerHTML = 'Lat/Lon: '  + position.coords.latitude + ' ' + position.coords.longitude + '<br />';
+    statusElement.setAttribute('style', 'background-color: green;');
 
     Latitude = position.coords.latitude;
     Longitude = position.coords.longitude;
 
     getMap(Latitude, Longitude);
-
-    var parsedData = JSON.stringify(dummyData);
-
-    console.log("Sending GPS data: " + parsedData);
-
-    // Sending and receiving data in JSON format using POST mothod
-    $.ajax({
-        type       : "POST",
-        url        : urlObuGateway,
-        crossDomain: true,
-        data       : parsedData,
-        dataType   : "json",
-        contentType: 'application/json; charset=utf-8',
-        success    : function(response) {
-            console.log("Response: %O", response);
-            console.log("Response: " + JSON.stringify(response));
-        },
-        error      : function() {
-            alert('Not working!');                  
-        }
-    });
 
 };
 
@@ -148,5 +159,78 @@ function getMap(latitude, longitude) {
     map.setZoom(15);
     map.setCenter(marker.getPosition());
 }
+
+//
+// function to GPS send data, if new data is available
+// 
+function sendData() {
+
+    if (gpsPositions.length > 0) {
+        var jsonPositions = JSON.stringify(gpsPositions);
+
+        idCounter += 1;
+        var sendData = {
+            "id": idCounter,
+            "jsonrpc":"2.0",
+            "method":"sendPos",
+            "params":{
+                "contractId":contractId,
+                "obuid":obuId,
+                "pos":jsonPositions
+            }
+        };
+
+        var jsonData = JSON.stringify(sendData);
+        console.log("Sending GPS data: " + jsonData);
+
+        // Sending and receiving data in JSON format using POST mothod
+        $.ajax({
+            type       : "POST",
+            url        : urlObuGateway,
+            crossDomain: true,
+            data       : jsonData,
+            dataType   : "json",
+            contentType: 'application/json; charset=utf-8',
+            success    : sendDataSuccess,
+            error      : sendDataError,
+        });
+    }
+    else {
+        console.log("Nothing to send");
+    }
+}
+
+function sendDataSuccess(response) {
+    console.log("sendData successfully returned %O", response);
+
+    if ("error" in response) {
+        console.log("ERROR " + response["error"]["code"] + " : " + response["error"]["message"]["message"]);
+        var statusElement = document.getElementById('sendcounter');
+        statusElement.innerHTML = 'Send count:' + idCounter;
+        statusElement.setAttribute('style', 'background-color: red;');
+    } else if ("result" in response) {
+        console.log("got response code " + response["result"] + " for id " + response["id"]);
+        if (response["id"] == idCounter && response["result"] == 0) {
+            gpsPositions = [];
+            
+            var statusElement = document.getElementById('sendcounter');
+            statusElement.innerHTML = 'Send count:' + idCounter;
+            statusElement.setAttribute('style', 'background-color: green;');
+        }
+    } else {
+        console.log("ERROR : invalid response, cannot parse response!")
+        var statusElement = document.getElementById('sendcounter');
+        statusElement.innerHTML = 'Send count:' + idCounter;
+        statusElement.setAttribute('style', 'background-color: red;');
+    }
+}
+
+function sendDataError(e) {
+    var statusElement = document.getElementById('sendcounter');
+    statusElement.innerHTML = 'Send count:' + idCounter;
+    statusElement.setAttribute('style', 'background-color: red;');
+    console.log(e);
+}
+
 
 app.initialize();
